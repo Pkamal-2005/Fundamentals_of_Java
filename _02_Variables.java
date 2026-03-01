6390face09b3c0e1400d5ba5b64029c7ff10e8fa
Kai <pkamallochan0@gmail.com>
2026-03-01 15:07:48 +0530

    Variables Added

--- _02_Variables.java
+++ _02_Variables.java
@@ -7,6 +7,21 @@ class _02_Variables{
 
         int num = 124;
         System.out.println(num);
+
+        float marks = 9.5f ;
+        byte by = 11;
+        short sh = 333;
+        long l = 5555l;
+        char c = 'K';
+        String name = "Kai";
+        boolean b = true;
+        System.out.println(marks);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(name);
+        System.out.println(b);
     }
 }
 
